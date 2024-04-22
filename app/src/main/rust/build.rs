8e13f32046e331d320300e8bc8624311edad9c21
weixian.zhou <weixian.zhou@ninebot.com>
2024-04-22 20:54:44 +0800

    add ld to build.rs

--- app/src/main/rust/build.rs
+++ app/src/main/rust/build.rs
@@ -1,5 +1,9 @@
 
 
 fn main() {
-    println!("Hello, world!");
+        // 告诉编译器去指定路径下查找共享库文件
+        println!("cargo:rustc-link-search=/Users/leochou/Github/flutter/flutter_btleplug3/enc/target/android/arm64-v8a/");
+    
+        // 告诉编译器链接到指定的共享库文件
+        println!("cargo:rustc-link-lib=nbenc_ffi");
 }
